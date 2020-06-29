56b5e32eb8a0e38fc1b845d86aeff3505cc3f988
eriklangille <eriklangille@gmail.com>
2020-06-28 20:25:54 -0700

    Fixed CORS bug

--- server/index.js
+++ server/index.js
@@ -13,18 +13,18 @@ const keys = require("./config/keys.js");
 
 const port = 5000;
 
-var whitelist = ['https://eriklangille.com', keys.localIP]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+var whitelist = ['https://eriklangille.com', keys.localIP, "http://localhost:3000/"]
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
   }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 
 app.use(
