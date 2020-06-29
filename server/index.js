f19e92866d64f791844101c1d188480f0026f18f
eriklangille <eriklangille@gmail.com>
2020-06-28 20:01:40 -0700

    Restricted CORS

--- server/index.js
+++ server/index.js
@@ -9,10 +9,22 @@ const users = require("./routes/api/users.js");
 const blog = require("./routes/api/blog.js");
 const upload = require("./routes/api/upload.js");
 const projects = require("./routes/api/projects.js");
+const keys = require("../config/keys.js");
 
 const port = 5000;
 
-app.use(cors());
+var whitelist = ['https://eriklangille.com', keys.localIP]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(
--- setup.md
+++ setup.md
@@ -4,6 +4,7 @@
 * SSH into the aws server using the .ppk file for auth
 * Navigate to /apps/el-website/
 * Turn off the server by using "forever stopall"
+* If something still running on a port (ie 3000) then the pid can be found from "netstat -nlp" and then use "kill [pid]"
 * git pull the latest version from the git repo
 * cd client and start with "forever start -c "npm run start" ./"
 * cd server and start with "forever start index.js"
